61dfeddddf187c13bb02f25174839b4a882c2dbd
Giovanni Antonaccio <giovanniantonaccio@gmail.com>
2019-08-12 19:40:26 -0400

    add action to increment and decrement the amount

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -18,11 +18,22 @@ export default function cart(state = [], action) {
     case '@cart/REMOVE':
       return produce(state, draft => {
         const productIndex = draft.findIndex(p => p.id === action.id);
-        console.tron.log(productIndex);
         if (productIndex >= 0) {
           draft.splice(productIndex, 1);
         }
       });
+    case '@cart/UPDATE_AMOUNT': {
+      if (action.amount <= 0) {
+        return state;
+      }
+      return produce(state, draft => {
+        const productIndex = draft.findIndex(p => p.id === action.id);
+
+        if (productIndex >= 0) {
+          draft[productIndex].amount = Number(action.amount);
+        }
+      });
+    }
     default:
       return state;
   }
